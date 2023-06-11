b5906608aed66d543bf15a957ee3200d2152dfaf
Metin Keleş <metinkeles677@gmail.com>
2023-06-11 22:04:16 +0300

    testNetlify

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Metin Keleş</h1>
         <Navbar icon="bi bi-github" title="Github Finder" />
         <Search searchUsers={this.searchUsers} />
         <div className='container mb-3'>
